2145ba29c49bcd8af4e64b91982e830db30a9511
David Mehren <git@herrmehren.de>
2026-01-12 15:48:53 +0100

    Add repro

---
+++ Tests.cs
@@ -0,0 +1,26 @@
+﻿namespace TUnitIgnoreTypeRepro;
+
+public class Tests
+{
+    public record IgnoreMe(string Message);
+    
+    public record TypeOne(IgnoreMe IgnoreOne, IgnoreMe IgnoreTwo);
+    public record TypeTwo((IgnoreMe, IgnoreMe) Ignores);
+    
+    [Test]
+    public async Task Works()
+    {
+        var actual = new TypeOne(new IgnoreMe("foobar"), new IgnoreMe("foobar"));
+        var expected = new TypeOne(new IgnoreMe("baz"), new IgnoreMe("baz"));
+        
+        await Assert.That(actual).IsEquivalentTo(expected).IgnoringType<IgnoreMe>();
+    }
+    [Test]
+    public async Task Fails()
+    {
+        var actual = new TypeTwo((new IgnoreMe("foobar"), new IgnoreMe("foobar")));
+        var expected = new TypeTwo((new IgnoreMe("baz"), new IgnoreMe("baz")));
+        
+        await Assert.That(actual).IsEquivalentTo(expected).IgnoringType<IgnoreMe>();
+    }
+}
